469310ebdee6713a950bb6f8acf0d019ad3d696d
Wojciech Wyskiel <wojtek12.1.96@wp.pl>
2023-10-26 12:41:15 +0200

    Done class 'active' in clicked link

--- js/script.js
+++ js/script.js
@@ -2,14 +2,29 @@
 
 
 function titleClickHandler(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-    /* add class 'active' to the clicked link */
+    for(let activeLink of activeLinks){
+     activeLink.classList.remove('active');
+    }
 
-    /* remove class 'active' from all articles */
+    /* [DONE] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+
+    console.log('clickedElement:', clickedElement);
+    console.log('clickedElement (with plus): ' + clickedElement);
+
+
+    /* [DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts .post');
+
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
 
     /* get 'href' attribute from the clicked link */
 
